2d29cc073cccd50a8e7fae4f61a9032e1423f060
Anakin Lee <1286130293@qq.com>
2018-07-05 19:18:28 +0800

    first commit

---
+++ Backup/PluginSDK/AltitudeMode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WorldWind
+{
+	public enum AltitudeMode
+	{
+		ClampedToGround,
+		RelativeToGround,
+		Absolute
+	}
+}
---
+++ Timeline/LatLonGrid.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Xml;
+
+namespace Timeline
+{
+	internal sealed class LatLonGrid : ToggleBase
+	{
+		public LatLonGrid(XmlNode node) : base(node) {}
+	}
+}
